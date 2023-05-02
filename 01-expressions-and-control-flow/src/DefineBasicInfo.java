f09eba6e2f1fe1480e716b8e688121501d0ea250
ildi-laszak <ildi.laszak@protonmail.com>
2023-05-02 16:08:21 +0200

    add DefineBasicInfo.java

---
+++ 01-expressions-and-control-flow/src/DefineBasicInfo.java
@@ -0,0 +1,15 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myName = "Ildi";
+        int myAge = 44;
+        double myHeight = 1.68;
+        boolean isMarried = false;
+        System.out.println("Name: " + myName + ", age: " + myAge + ", height: " + myHeight + ", marriage status: " + isMarried);
+    }
+}
+
+// Define several things as a variable then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a double
+// Whether you are married or not as a boolean

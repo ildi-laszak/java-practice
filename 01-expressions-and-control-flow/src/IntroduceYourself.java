cb3785fa48dd05a5d3b19c93a7252e6b82e90a71
ildi-laszak <ildi.laszak@protonmail.com>
2023-05-02 15:06:04 +0200

    add IntroduceYourself.java

---
+++ 01-expressions-and-control-flow/src/IntroduceYourself.java
@@ -0,0 +1,22 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        String name = "Ildi Laszak";
+        int age = 43;
+        double height = 1.68;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
+
+// Write a program that prints a few details to the terminal window about you
+// It should print each detail to a new line:
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87

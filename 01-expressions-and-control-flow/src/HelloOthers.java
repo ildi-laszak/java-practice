e8613188b6d31d72b78ef2f02f747b43a24f3f74
ildi-laszak <ildi.laszak@protonmail.com>
2023-05-02 14:54:52 +0200

    add HelloOthers.java

---
+++ 01-expressions-and-control-flow/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Stede!");
+        System.out.println("Hello, Ed!");
+        System.out.println("Hello, Lucius!");
+    }
+}
+// Greet 3 of your classmates with this program in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!

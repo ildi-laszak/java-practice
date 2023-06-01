aa407382bdc23d4fe2e83e307271e5bd65883cb5
ildi-laszak <ildi.laszak@protonmail.com>
2023-06-01 17:36:01 +0200

    add SumElements.java

---
+++ 02-arrays/src/SumElements.java
@@ -0,0 +1,11 @@
+public class SumElements {
+    public static void main(String[] args) {
+        int[] numbers = {54, 23, 66, 12};
+        int sumElements = numbers[1] + numbers[2];
+        System.out.println(sumElements);
+    }
+}
+
+// - Create an array variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element

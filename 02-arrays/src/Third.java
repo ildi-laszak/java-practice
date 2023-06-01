a5297b7592dbab5f4882e395b4e93b14c66408b4
ildi-laszak <ildi.laszak@protonmail.com>
2023-06-01 17:25:39 +0200

    add Third.java

---
+++ 02-arrays/src/Third.java
@@ -0,0 +1,11 @@
+public class Third {
+    public static void main(String[] args) {
+
+        int[] numbers = {4, 5, 6, 7};
+        System.out.println(numbers[2]);
+    }
+}
+
+// Create an array variable named `numbers`
+// with the following content: `[4, 5, 6, 7]`
+// Print the third element of `numbers`
